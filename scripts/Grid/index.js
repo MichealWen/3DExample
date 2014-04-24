7bbaf028c2a4c338c291aeae3510a0a43ea007cf
SoldovskijBB <soldovskij@hotmail.com>
2014-04-24 17:28:50 +0400

    search by x, y

--- scripts/Grid/index.js
+++ scripts/Grid/index.js
@@ -3,6 +3,7 @@
  */
 'use strict';
 
+//region base
 // STATS
 var stats = new Stats();
 stats.setMode(0);
@@ -18,7 +19,7 @@ scene = new THREE.Scene();
 
 // CAMERA
 camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, 10000);
-camera.position.set(0, 50, 0);
+camera.position.set(0, 0, 50);
 camera.lookAt(scene.position);
 
 // RENDERER
@@ -47,40 +48,82 @@ controls = new THREE.OrbitControls(camera, renderer.domElement);
 var floorGeometry = new THREE.PlaneGeometry(10, 10, 1, 1);
 var floors = [];
 
-for (var i = 0; i < 9; i++) {
+for (var i = 0; i < 18; i++) {
     floors.push(new THREE.Mesh(floorGeometry, new THREE.MeshBasicMaterial({ color: 0x119922, side: THREE.DoubleSide })));
 }
 
 floors[0].position.x = -10;
-floors[0].position.z = -10;
+floors[0].position.y = -10;
+floors[0].material.color.setRGB (0.2, 0.8, 0.2);
 
 floors[1].position.x = 0;
-floors[1].position.z = -10;
+floors[1].position.y = -10;
+floors[1].material.color.setRGB (0.2, 0.8, 0.2);
 
 floors[2].position.x = 10;
-floors[2].position.z = -10;
+floors[2].position.y = -10;
+floors[2].material.color.setRGB (0.2, 0.8, 0.2);
 
 floors[3].position.x = -10;
-floors[3].position.z = 0;
+floors[3].position.y = 0;
+floors[3].material.color.setRGB (0.2, 0.8, 0.2);
 
 floors[4].position.x = 0;
-floors[4].position.z = 0;
+floors[4].position.y = 0;
+floors[4].material.color.setRGB (0.2, 0.8, 0.2);
 
 floors[5].position.x = 10;
-floors[5].position.z = 0;
+floors[5].position.y = 0;
+floors[5].material.color.setRGB (0.2, 0.8, 0.2);
 
 floors[6].position.x = -10;
-floors[6].position.z = 10;
+floors[6].position.y = 10;
+floors[6].material.color.setRGB (0.2, 0.8, 0.2);
 
 floors[7].position.x = 0;
-floors[7].position.z = 10;
+floors[7].position.y = 10;
+floors[7].material.color.setRGB (0.2, 0.8, 0.2);
 
 floors[8].position.x = 10;
-floors[8].position.z = 10;
+floors[8].position.y = 10;
+floors[8].material.color.setRGB (0.2, 0.8, 0.2);
 
-for (var i = 0; i < floors.length; i++) {
-    floors[i].rotation.x = Math.PI / 2;
-}
+
+floors[9].position.x = -10;
+floors[9].position.y = -10;
+floors[9].position.z = 10;
+
+floors[10].position.x = 0;
+floors[10].position.y = -10;
+floors[10].position.z = 10;
+
+floors[11].position.x = 10;
+floors[11].position.y = -10;
+floors[11].position.z = 10;
+
+floors[12].position.x = -10;
+floors[12].position.y = 0;
+floors[12].position.z = 10;
+
+floors[13].position.x = 0;
+floors[13].position.y = 0;
+floors[13].position.z = 10;
+
+floors[14].position.x = 10;
+floors[14].position.y = 0;
+floors[14].position.z = 10;
+
+floors[15].position.x = -10;
+floors[15].position.y = 10;
+floors[15].position.z = 10;
+
+floors[16].position.x = 0;
+floors[16].position.y = 10;
+floors[16].position.z = 10;
+
+floors[17].position.x = 10;
+floors[17].position.y = 10;
+floors[17].position.z = 10;
 
 for (var i = 0; i < floors.length; i++) {
     scene.add(floors[i]);
@@ -93,6 +136,16 @@ var state;
 
 window.addEventListener('mousemove', onMouseMove, false);
 window.addEventListener('keydown', onKeyDown, false);
+
+function addLog(string){
+    var log = document.getElementById('log');
+    log.innerText += string + '\n';
+}
+
+function clearLog(){
+    var log = document.getElementById('log');
+    log.innerText = '';
+}
 
 function onKeyDown(event) {
     // KeyCode 32 (SPACE)
@@ -112,32 +165,74 @@ function onMouseMove(event) {
     // Получаем значение y от -1 до 1
     var y = -( event.clientY / window.innerHeight ) * 2 + 1;
 
-    // создаем некий вектор с этими значениями
+    // Создаем некий вектор с этими значениями
     var vector = new THREE.Vector3(x, y, 0);
 
-    // Преобразуем координаты мыши в координаты мира
+    clearLog();
+    addLog('Origin');
+    addLog(vector.x);
+    addLog(vector.y);
+    addLog(vector.z);
+
+
+    // Меняем положение вектора на положение камеры
     projector.unprojectVector(vector, camera);
+
+    addLog('Unproject');
+    addLog(vector.x);
+    addLog(vector.y);
+    addLog(vector.z);
 
     // Вычитаем из вектора позицию камеры и нормализуем
     var direction = vector.sub(camera.position).normalize();
 
-    // Узнаем дистанцию между камерой и направлением ?
-    var distance = -camera.position.y / direction.y;
+    addLog('Normal Direction');
+    addLog(vector.x);
+    addLog(vector.y);
+    addLog(vector.z);
 
-    // Конечная позиция
+
+    // Узнаем дистанцию между камерой и направлением ?
+    var distance = -camera.position.z / direction.z;
+
+    addLog('Distance');
+    addLog(distance);
+
+
+    // Умножаем направление на дистанцию
     var diff = direction.multiplyScalar(distance);
+
+    addLog('Diff');
+    addLog(diff.x);
+    addLog(diff.y);
+    addLog(diff.z);
+
+    // Берем позицию камеры
     var cpos = camera.position.clone();
+
+    addLog('cPos');
+    addLog(cpos.x);
+    addLog(cpos.y);
+    addLog(cpos.z);
+
+    // К поцизии камеры добавляем разницу
     var position = cpos.add(diff);
 
+    addLog('Position');
+    addLog(position.x);
+    addLog(position.y);
+    addLog(position.z);
+
+
     if (state) {
-        if (position.x >= -15 && position.x <= 15 && position.z >= -15 && position.z <= 15) {
+        if (position.x >= -15 && position.x <= 15 && position.y >= -15 && position.y <= 15) {
 
             for (var i = 0; i < floors.length; i++) {
-                var top = floors[i].position.z - 5;
+                var top = floors[i].position.y - 5;
                 var left = floors[i].position.x - 5;
                 var right = floors[i].position.x + 5;
-                var bottom = floors[i].position.z + 5;
-                if (position.x >= left && position.x <= right && position.z >= top && position.z <= bottom) {
+                var bottom = floors[i].position.y + 5;
+                if (position.x >= left && position.x <= right && position.y >= top && position.y <= bottom) {
                     floors[i].material.color.setRGB (0, 0, 1);
                 } else {
                     floors[i].material.color.setRGB (0.5, 1, 0);
