3fd65ec22288aace828f9a84d339e5b6de0b32ef
SoldovskijBB <soldovskij@hotmail.com>
2014-04-26 15:38:43 +0400

    Базовое передвижение по найденному пути, код вообще не оптимизирован

--- scripts/Grid/index.js
+++ scripts/Grid/index.js
@@ -18,8 +18,16 @@ var scene = new THREE.Scene();
 
 // CAMERA
 var camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, 10000);
-camera.position.set(0, 0, 55);
-camera.lookAt(scene.position);
+camera.position.set(0, 0, 60);
+camera.up = new THREE.Vector3(0, 0, 1);
+
+var cameraCenter = new THREE.Vector3(0, 0, 60);
+var cameraRotate = new THREE.Vector3(50, -50, 0);
+
+var cameraTarget = new THREE.Mesh(new THREE.CubeGeometry(1, 1, 1), new THREE.MeshLambertMaterial({ color: 0xffffff }))
+cameraTarget.position.copy(cameraRotate);
+
+scene.add(cameraTarget);
 
 // RENDERER
 var renderer = new THREE.WebGLRenderer({antialias: true});
@@ -37,22 +45,20 @@ var skyBoxMaterial = new THREE.MeshBasicMaterial({ color: 0x9999ff, side: THREE.
 var skyBox = new THREE.Mesh(skyBoxGeometry, skyBoxMaterial);
 scene.add(skyBox);
 
-// CONTROLS
-var controls = new THREE.OrbitControls(camera, renderer.domElement);
-
 //endregion
 
 //region FLOOR
 var floorGeometry = new THREE.PlaneGeometry(5, 5, 1, 1);
 
 // Размерность массива
-var floorX = 100;
-var floorY = 100;
+var floorX = 30;
+var floorY = 30;
 
 // Сетка для библиотеки поиска пути
 var PFgrid = new PF.Grid(floorX, floorY);
 // Алгоритм поиска пути
 var PFFinder = new PF.AStarFinder({allowDiagonal: true});
+//var PFFinder = new PF.AStarFinder({allowDiagonal: false});
 
 // Создаем двумерный массив
 var floors = new Array(floorX);
@@ -63,7 +69,7 @@ for (var i = 0; i < floorX; i++) {
 // Заполняем двумерный массив
 for (var i = 0; i < floorX; i++) {
     for (var j = 0; j < floorY; j++) {
-        floors[i][j] = new THREE.Mesh(floorGeometry, new THREE.MeshBasicMaterial({ color: 0x119922, side: THREE.DoubleSide }));
+        floors[i][j] = new THREE.Mesh(floorGeometry, new THREE.MeshBasicMaterial({ color: 0x119922, side: THREE.FrontSide }));
         floors[i][j].position.x = i * 5;
         floors[i][j].position.y = j * -5;
         floors[i][j].userData = {'x': i, 'y': j};
@@ -77,7 +83,47 @@ var projector = new THREE.Projector();
 var raycaster;
 var cursor;
 
+var PlayerGridPosition = new THREE.Vector2(15, 15);
+var PlayerObject = new THREE.Mesh(new THREE.CubeGeometry(5, 5, 5), new THREE.MeshLambertMaterial({ color: 0xffffff }))
+PlayerObject.position.setX(floors[PlayerGridPosition.x][PlayerGridPosition.y].position.x);
+PlayerObject.position.setY(floors[PlayerGridPosition.x][PlayerGridPosition.y].position.y);
+PlayerObject.position.setZ(2.5);
+var PFPath;
+var RoadPoints = [];
+var StepIndex = 0;
+var STATE = {NONE: 0, MOVE: 1}
+var state = STATE.NONE;
+
+scene.add(PlayerObject);
 window.addEventListener('mousemove', onMouseMove, false);
+window.addEventListener('keydown', onKeyDown, false);
+window.addEventListener('mousedown', onMouseDown, false);
+
+function onMouseDown(event) {
+    if (event.button === 0 && PFPath && PFPath.length > 1) {
+        RoadPoints.splice(0, RoadPoints.length);
+
+        var geometry = new THREE.Geometry();
+
+
+        for (var i = 1; i < PFPath.length; i++) {
+            RoadPoints.push( {
+                'world' : new THREE.Vector3(floors[PFPath[i][0]][PFPath[i][1]].position.x, floors[PFPath[i][0]][PFPath[i][1]].position.y, 2.5),
+                'grid' : {'x' : PFPath[i][0], 'y' : PFPath[i][1]}
+            });
+        }
+
+        for(var i = 0; i < RoadPoints.length; i++) {
+            geometry.vertices.push(RoadPoints[i].world);
+        }
+
+        var material = new THREE.LineBasicMaterial({ color: 0x000000, opacity: 0.5 });
+        var line = new THREE.Line(geometry, material);
+        scene.add(line);
+
+        state = STATE.MOVE;
+    }
+}
 
 function onMouseMove(event) {
     // Получаем значение x от -1 до 1
@@ -102,22 +148,85 @@ function onMouseMove(event) {
                 cursor = intercasts[0].object;
 
                 // Ищем как пройти от 0,0 до того, где курсор
-                var PFPath = PFFinder.findPath(0, 0, cursor.userData.x, cursor.userData.y, PFgrid.clone());
+                PFPath = PFFinder.findPath(PlayerGridPosition.x, PlayerGridPosition.y, cursor.userData.x, cursor.userData.y, PFgrid.clone());
 
-                for (var k = 0; k < floorX; k++) {
-                    for (var j = 0; j < floorY; j++) {
-                        floors[k][j].material.color.setHex(0x119922);
+                for (var x = 0; x < floorX; x++) {
+                    for (var y = 0; y < floorY; y++) {
+                        floors[x][y].material.color.setHex(0x119922);
                     }
                 }
 
-                for(var i = 0; i < PFPath.length; i++){
-                    var cell = PFPath[i];
-                    floors[cell[0]][cell[1]].material.color.setHex(0xff0022);
+                var stepLenght = 5;
+                if (PFPath.length < stepLenght) {
+                    for (var d = 0; d < PFPath.length; d++) {
+                        var cell = PFPath[d];
+                        floors[cell[0]][cell[1]].material.color.setHex(0xff0022);
+                    }
+                } else {
+                    for (var d = 0; d < stepLenght; d++) {
+                        var cell = PFPath[d];
+                        floors[cell[0]][cell[1]].material.color.setHex(0xff0022);
+                    }
+
+                    for (var d = stepLenght; d < PFPath.length; d++) {
+                        var cell = PFPath[d];
+                        floors[cell[0]][cell[1]].material.color.setHex(0xffFF22);
+                    }
                 }
             }
         }
     }
 }
+
+function onKeyDown(event) {
+    // KeyCode 87 (W)
+    if (event.keyCode == 87) {
+        cameraCenter.y -= 2;
+        cameraRotate.y -= 2;
+    }
+
+    // KeyCode 83 (S)
+    if (event.keyCode == 83) {
+        cameraCenter.y += 2;
+        cameraRotate.y += 2;
+    }
+
+    // KeyCode 65 (A)
+    if (event.keyCode == 65) {
+        cameraCenter.x += 2;
+        cameraRotate.x += 2;
+    }
+
+    // KeyCode 68 (D)
+    if (event.keyCode == 68) {
+        cameraCenter.x -= 2;
+        cameraRotate.x -= 2;
+    }
+
+    // KeyCode 81 (Q)
+    if (event.keyCode == 81) {
+        cameraCenter.z -= 2;
+    }
+
+    // KeyCode 69 (E)
+    if (event.keyCode == 69) {
+        cameraCenter.z += 2;
+    }
+
+    // KeyCode 82 (R)
+    if (event.keyCode == 82) {
+        cameraRotate.x += 2;
+        cameraRotate.y -= 2;
+    }
+
+    // KeyCode 70 (F)
+    if (event.keyCode == 70) {
+        cameraRotate.x -= 2;
+        cameraRotate.y += 2;
+    }
+
+}
+
 
 animate();
 
@@ -133,7 +242,34 @@ function animate() {
 }
 
 function update() {
-    controls.update();
+
+    camera.position.setX(cameraCenter.x);
+    camera.position.setY(cameraCenter.y);
+    camera.position.setZ(cameraCenter.z);
+
+    camera.lookAt(cameraRotate);
+    cameraTarget.position.copy(cameraRotate);
+
+    if (state == STATE.MOVE) {
+        if (RoadPoints.length > 0) {
+            if (PlayerObject.position.distanceTo(RoadPoints[StepIndex].world) > 0.2) {
+                PlayerObject.lookAt(RoadPoints[StepIndex].world);
+                PlayerObject.translateOnAxis(new THREE.Vector3(0, 0, 1), 0.2);
+                console.log('q')
+            } else {
+                if (RoadPoints.length > StepIndex) {
+                    console.log('e')
+                    PlayerGridPosition.x = RoadPoints[StepIndex].grid.x;
+                    PlayerGridPosition.y = RoadPoints[StepIndex].grid.y;
+                    StepIndex++;
+                    if (StepIndex == RoadPoints.length) {
+                        state = STATE.NONE;
+                        StepIndex = 0;
+                    }
+                }
+            }
+        }
+    }
 }
 
 function render() {
